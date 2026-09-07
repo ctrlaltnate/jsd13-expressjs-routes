5ab4da02019d9aba05b65933f77812d94f91cbdc
ctrlaltname <ekaluck.musik@gmail.com>
2026-09-07 14:44:05 +0700

    rest api db test update and delete

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
     {
         username : { type: String, required: true },
-        email: { type: String, required: true , },
+        email: { 
+            type: String, 
+            required: true , 
+            unique: true, 
+            lowercase: true, 
+            match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"],
+            trim: true
+        },
         password: { type: String, required: true, select: false },
     },
     {
--- backend/src/routes/v2/users.routes.js
+++ backend/src/routes/v2/users.routes.js
@@ -34,16 +34,46 @@ router.post("/", async(req, res ,next) => {
 });
 
 // Update users
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+    }).select("-password");
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    return res.status(200).json(updatedUser);
+
+
   } catch (err) {
     next(err);
   }
 });
 
 // Delete users
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" });
+    
   } catch (err) {
     next(err);
   }
